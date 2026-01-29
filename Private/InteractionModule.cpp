030b052fe225e02cdcfdce97fb01144e31a5c74d
DobrBobr <kindom.bobr@gmail.com>
2026-01-29 20:02:25 +0100

    Init commit with empty UE module.

---
+++ Private/InteractionModule.cpp
@@ -0,0 +1,19 @@
+﻿#include "InteractionModule.h"
+
+#define LOCTEXT_NAMESPACE "FInteractionModuleModule"
+
+DEFINE_LOG_CATEGORY(LogInteractionModule)
+
+void FInteractionModule::StartupModule()
+{
+    
+}
+
+void FInteractionModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FInteractionModule, InteractionModule)
